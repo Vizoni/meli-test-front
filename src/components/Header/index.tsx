dc9a8a3d41581ce99cf415a8993dab0eeb1471ce
Raphael Vizoni do Prado <ravizoni@gmail.com>
2022-07-01 22:37:51 -0300

    redirect to home from header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import SearchBar from "./../SearchBar";
 import { Container, Logo } from "./styled";
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+	const navigate = useNavigate();
+	function goToHome() {
+		navigate(`/`)
+	}
 	return (
 		<Container>
-			<Logo>
+			<Logo onClick={goToHome}>
 				<img src="/icons/Logo_ML@2x.png.png" />
 			</Logo>
 			<SearchBar />
--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -3,20 +3,16 @@ import styled from "styled-components";
 export const Container = styled.header`
 	background-color: var(--yellow);
 	height: 5vh;
-	/* width: 100%; */
 	display: flex;
 	justify-content: center;
 	flex-flow: row;
-	/* padding: 0 5rem; */
 `;
 
 export const Logo = styled.div`
 	align-self: end;
+	cursor: pointer;
 
 	img {
 		height: 4vh;
-		/* margin: 0.5rem; */
-		/* width: 10%;
-		max-width: 10%; */
 	}
 `;
